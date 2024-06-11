a9632cd9f3ed03b40b53f80990e33cf2df248809
LizManchego <karinamanchego0416@gmail.com>
2024-06-11 12:34:36 -0600

    ADD Tema 5 Desktop / Mobile / Animation FULL

--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -7,103 +7,97 @@
       .titulo-principal__numero
         span 5
       h1 Proceso de investigación
-    p.mb-5 El desarrollo de una investigación debe necesariamente tener una estructura sólida, coherente y organizada. Esto, con el fin obtener la información y los resultados de la forma más eficiente posible.
-    .bloque-texto-a.color-secundario.p-4.p-md-5.mb-5(data-aos="slide-left")
-      .row.m-0.align-items-center.justify-content-between
-        .col-lg-2.mb-4.mb-lg-0
-          figure
-            img(src='@/assets/curso/img1.svg', style="max-width: 60%; margin: auto;")
-        .col-lg-10
-          .bloque-texto-a__texto.p-4
-            p.mb-0 El proceso de la investigación de mercados comprende una serie de etapas que encadenadas generan un proceso de información para la toma de decisiones; además, están comprendidas de la siguiente manera: identificación del problema, metodología, recolección de datos, análisis, conclusiones y estrategias.
-    separador.mb-5
-    #t_5_1
-    .titulo-segundo
-      h2 5.1 Identificación del problema
-    p.mb-4 El problema de investigación se define con base en la información que se desea recolectar y el origen mismo del problema, ya sea para indagar acerca de la conducta de compra de determinado producto, el nivel de participación en el mercado o las características de los canales de distribución.
-    .row.align-items-center
-      .col-lg-6
-        .cajon.color-primario.p-4.mb-4
-          p.mb-0 El problema define la información general que la empresa busca obtener con el desarrollo de la investigación; en este punto, lo más recomendable es hacerlo en forma de pregunta. Para ello, se deben reunir algunas características puntuales:
-        ul.lista-ul--separador.mb-4
+    p.mb-5(data-aos="zoom-in-left") El desarrollo de una investigación debe necesariamente tener una estructura sólida, coherente y organizada. Esto, con el fin obtener la información y los resultados de la forma más eficiente posible.
+    .row.justify-content-center(data-aos="zoom-in-left")
+      .col-sm-12.col-lg-10
+        .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5.mb-5
+          .bloque-texto-g__img.bgimg14
+          .bloque-texto-g__texto.p-5
+            p.mb-0  El proceso de la investigación de mercados comprende una serie de etapas que encadenadas generan un proceso de información para la toma de decisiones; además, están comprendidas de la siguiente manera: identificación del problema, metodología, recolección de datos, análisis, conclusiones y estrategias.
+    
+    Separador
+    #t_5_1.titulo-segundo.color-secundario(data-aos="zoom-in-left")
+      h2  5.1 Identificación del problema
+    p.mb-4(data-aos="zoom-in-left") El problema de investigación se define con base en la información que se desea recolectar y el origen mismo del problema, ya sea para indagar acerca de la conducta de compra de determinado producto, el nivel de participación en el mercado o las características de los canales de distribución.
+    
+    .tarjeta--container.tarjeta-radius-0.row.mb-5.justify-content-center(data-aos="zoom-in-left")
+      .col-sm-12.col-lg-6.tarjeta.bg4.px-5.py-5
+        p.mb-0 El problema define la información general que la empresa busca obtener con el desarrollo de la investigación; en este punto, lo más recomendable es hacerlo en forma de pregunta. Para ello, se deben reunir algunas características puntuales:
+        ul.lista-ul
           li 
-            i.fas.fa-caret-right(style="color: #37A3A5")
+            i.fas.fa-check-circle.color-primario
             | Ser abierta, que la respuesta conduzca a una explicación amplia y argumentada, en ningún momento debe generar una respuesta con un sí o un no.
           li 
-            i.fas.fa-caret-right(style="color: #37A3A5")
+            i.fas.fa-check-circle.color-primario
             | Ser concreta, ir al punto y preguntar directamente sin extensiones o introducciones innecesarias.
           li 
-            i.fas.fa-caret-right(style="color: #37A3A5")
+            i.fas.fa-check-circle.color-primario
             | Ser clara, cualquier persona que la lea debe tener la capacidad de entenderla.
           li 
-            i.fas.fa-caret-right(style="color: #37A3A5")
+            i.fas.fa-check-circle.color-primario
             | La pregunta debe conducir al diseño de los objetivos de la investigación.
-        p El problema de investigación es la información básica a recolectar; por lo tanto, este debe transformarse en el objetivo general del proyecto.
-      .col-lg-6
-        figure(data-aos="flip-right")
-          img(src='@/assets/curso/img1.svg', style="max-width: 60%; margin: auto;")
-    p.mt-4.mt-lg-0 Los objetivos específicos deben derivarse del objetivo general dividiéndolo en temas puntuales que conduzcan en su conjunto a la obtención de la información total.
-    separador.mb-5
-    #t_5_2
-    .titulo-segundo
-      h2 5.2 Metodología
-    .bloque-texto-a.color-secundario.p-4.p-md-5(data-aos="slide-right")
-      .row.m-0.align-items-center.justify-content-between
-        .col-lg-3.mb-4.mb-lg-0
-          figure
-            img(src='@/assets/curso/img1.svg', style="max-width: 80%; margin: auto;")
-        .col-lg-9
-          .bloque-texto-a__texto.p-4
-            p.mb-0 La metodología de la investigación se refiere a la forma como se va a recolectar la información; es decir, la selección de las fuentes, el tamaño de la muestra y los métodos que se van a utilizar para obtener los datos necesarios para cumplir con los objetivos general y específicos.
-    separador.mb-5
-    #t_5_3
-    .titulo-segundo
-      h2 5.3 Recolección de datos
-    .bloque-texto-a.color-tercero.p-4.p-md-5(data-aos="slide-left")
-      .row.m-0.align-items-center.justify-content-between
-        .col-lg-8.mb-4.mb-lg-0
-          .bloque-texto-a__texto.p-4
-            p.mb-0 Obtener la información se convierte en la parte más importante del proceso, puesto que estos datos serán necesarios para la posterior toma de decisiones. En este punto, a partir de un cronograma de trabajo detallado se aplican los diferentes instrumentos: encuestas, focus group, observación, entre otros. La logística en esta fase es importante debido a que son necesarios recursos técnicos, tecnológicos y humanos.
-        .col-lg-4
-          figure
-            img(src='@/assets/curso/img1.svg', style="max-width: 100%; margin: auto;")
-    separador.mb-5
-    #t_5_4
-    .titulo-segundo
-      h2 5.4 Análisis de información
-    .bloque-texto-a.color-secundario.p-4.p-md-5(data-aos="slide-right")
-      .row.m-0.align-items-center.justify-content-between
-        .col-lg-3.mb-4.mb-lg-0
-          figure
-            img(src='@/assets/curso/img1.svg', style="max-width: 80%; margin: auto;")
-        .col-lg-9
-          .bloque-texto-a__texto.p-4
-            p.mb-0 Requiere de varios pasos, el primero es organizar los datos, luego es necesario tabularlos para convertirlos en información. En este punto también se hacen necesarios algunos recursos tecnológicos como software de análisis y hojas de cálculo para guardar la información de forma ordenada. Los datos deben ser interpretados para facilitar la toma de decisiones.
-    separador.mb-5
-    #t_5_5
-    .titulo-segundo
-      h2 5.5 Conclusiones
-    .bloque-texto-a.color-tercero.p-4.p-md-5(data-aos="slide-left")
-      .row.m-0.align-items-center.justify-content-between
-        .col-lg-8.mb-4.mb-lg-0
-          .bloque-texto-a__texto.p-4
-            p.mb-0 En esta fase, expertos o personas con formación en mercadeo y publicidad deben tomar la información, estudiarla y definir algunas conclusiones, la cual debe dar de cierta forma respuesta al problema de investigación. Las conclusiones de la investigación se requieren que sean recogidas en un informe ejecutivo, donde se incluyan los resultados del análisis de datos; se pueden expresar con base en los objetivos, el problema de investigación y los instrumentos de recolección utilizados.
-        .col-lg-4
-          figure
-            img(src='@/assets/curso/img1.svg', style="max-width: 60%; margin: auto;")
-    separador.mb-5
-    #t_5_6
-    .titulo-segundo
-      h2 5.6 Estrategias
-    .bloque-texto-a.color-secundario.p-4.p-md-5(data-aos="slide-right")
-      .row.m-0.align-items-center.justify-content-between
-        .col-lg-3.mb-4.mb-lg-0
-          figure
-            img(src='@/assets/curso/img1.svg', style="max-width: 80%; margin: auto;")
-        .col-lg-9
-          .bloque-texto-a__texto.p-4
-            p.mb-0 La realización de una investigación de mercados o publicitaria debe tener un fin estratégico. En este punto los encargados del mercado y las comunicaciones dentro de la empresa deben tomar tanto las conclusiones como la información generada en todo el proceso, para luego plantear actividades y acciones para su beneficio.
+      .col-sm-12.col-lg-6.tarjeta.bgimg15.p-3.d-none.d-lg-block
+        h2.mt-5.p-3 &nbsp;
 
+
+    .row.justify-content-center.align-items-center.mb-5(data-aos="zoom-in-left")
+      .col-sm-12.col-lg-12.bg1.p-4
+       .row.justify-content-center.align-items-center
+        .col-sm-12.col-lg-2.d-none.d-lg-block
+          img(src='@/assets/curso/temas/tema5/img3.svg', style="max-width: 60%; margin: auto;")
+        .col-sm-12.col-lg-10
+          p.mb-4.text-white.fw-bold El problema de investigación es la información básica a recolectar; por lo tanto, este debe transformarse en el objetivo general del proyecto.
+          p.mb-0.text-white.fw-bold  Los objetivos específicos deben derivarse del objetivo general dividiéndolo en temas puntuales que conduzcan en su conjunto a la obtención de la información total.
+    
+    Separador
+    #t_5_2.titulo-segundo.color-secundario(data-aos="zoom-in-left")
+      h2  5.2 Metodología
+    .row.justify-content-center.align-items-center.mb-5(data-aos="zoom-in-left")
+      .col-sm-12.col-lg-4
+        p.mb-0 La metodología de la investigación se refiere a la forma como se va a recolectar la información; es decir, la selección de las fuentes, el tamaño de la muestra y los métodos que se van a utilizar para obtener los datos necesarios para cumplir con los objetivos general y específicos.
+      .col-sm-12.col-lg-4.d-none.d-lg-block
+        img(src='@/assets/curso/temas/tema5/img4.png', alt='')
+   
+   
+    Separador
+    #t_5_3.titulo-segundo.color-secundario(data-aos="zoom-in-left")
+      h2 5.3 Recolección de datos
+    .row.justify-content-center.align-items-center.mb-5(data-aos="zoom-in-left")
+      .col-sm-12.col-lg-4.d-none.d-lg-block
+        img(src='@/assets/curso/temas/tema5/img5.png', alt='')
+      .col-sm-12.col-lg-4 
+        p.mb-0 Obtener la información se convierte en la parte más importante del proceso, puesto que estos datos serán necesarios para la posterior toma de decisiones. En este punto, a partir de un cronograma de trabajo detallado se aplican los diferentes instrumentos: encuestas, focus group, observación, entre otros. La logística en esta fase es importante debido a que son necesarios recursos técnicos, tecnológicos y humanos.
+      
+    
+    
+    Separador
+    #t_5_4.titulo-segundo.color-secundario(data-aos="zoom-in-left")
+      h2 5.4 Análisis de información
+    .row.justify-content-center.align-items-center.mb-5(data-aos="zoom-in-left")
+      .col-sm-12.col-lg-4
+        p.mb-0  Requiere de varios pasos, el primero es organizar los datos, luego es necesario tabularlos para convertirlos en información. En este punto también se hacen necesarios algunos recursos tecnológicos como software de análisis y hojas de cálculo para guardar la información de forma ordenada. Los datos deben ser interpretados para facilitar la toma de decisiones.
+      .col-sm-12.col-lg-4.d-none.d-lg-block
+        img(src='@/assets/curso/temas/tema5/img6.png', alt='')
+    
+    Separador
+    #t_5_5.titulo-segundo.color-secundario(data-aos="zoom-in-left")
+      h2  5.5 Conclusiones
+    .row.justify-content-center.align-items-center.mb-5(data-aos="zoom-in-left")
+      .col-sm-12.col-lg-4.d-none.d-lg-block
+        img(src='@/assets/curso/temas/tema5/img7.png', alt='')
+      .col-sm-12.col-lg-4 
+        p.mb-0  En esta fase, expertos o personas con formación en mercadeo y publicidad deben tomar la información, estudiarla y definir algunas conclusiones, la cual debe dar de cierta forma respuesta al problema de investigación. Las conclusiones de la investigación se requieren que sean recogidas en un informe ejecutivo, donde se incluyan los resultados del análisis de datos; se pueden expresar con base en los objetivos, el problema de investigación y los instrumentos de recolección utilizados.
+       
+    
+    
+    Separador
+    #t_5_6.titulo-segundo.color-secundario(data-aos="zoom-in-left")
+      h2  5.6 Estrategias
+    .row.justify-content-center.align-items-center.mb-5(data-aos="zoom-in-left")
+      .col-sm-12.col-lg-4
+        p.mb-0  La realización de una investigación de mercados o publicitaria debe tener un fin estratégico. En este punto los encargados del mercado y las comunicaciones dentro de la empresa deben tomar tanto las conclusiones como la información generada en todo el proceso, para luego plantear actividades y acciones para su beneficio.
+      .col-sm-12.col-lg-4.d-none.d-lg-block
+        img(src='@/assets/curso/temas/tema5/img8.png', alt='')
 </template>
 
 <script>
